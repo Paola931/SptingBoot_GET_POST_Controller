639f4b5fa3bc14393a3966d7324d6016557af70b
Paola931 <145440087+Paola931@users.noreply.github.com>
2024-01-12 21:22:03 +0100

    Correction

--- name_app/name_app/src/main/java/com/example/name_app/NameController.java
+++ name_app/name_app/src/main/java/com/example/name_app/NameController.java
@@ -3,14 +3,14 @@ package com.example.name_app;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/names")
 public class NameController {
     @GetMapping(path = "/name")
     public String name(
             @RequestParam(value = "name", required = true)String name){
         return "Ciao " + name + "!";
     }
-    @PostMapping("/name")
+    @PostMapping("/reversed")
     public String reverse(
             @RequestBody String name){
         StringBuilder revName = new StringBuilder(name);
